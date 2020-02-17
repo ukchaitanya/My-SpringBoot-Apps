c497af3730c3215dde1af3348282f49fb5d0296f
Chaitanya U K <uk.chaitanya121@gmail.com>
2020-02-17 20:25:47 +0530

    Changes to deploy to docker

---
+++ currency-exchange-service/bin/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple) values(10001,'USD','INR',70);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple) values(10002,'CAD','INR',65);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple) values(10003,'AED','INR',20);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple) values(10004,'BHD','INR',190);
